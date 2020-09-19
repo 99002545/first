edc4258f571ed8a96b81b9fdf3a5400fae4ca17e
99002545 <71481403+99002545@users.noreply.github.com>
2020-09-19 09:21:29 +0530

    Update main.c

--- calc/main.c
+++ calc/main.c
@@ -1,3 +1,21 @@
 
-@mainpage Calculator Application by Bharath G
-@subpage calculator_operations.h
+//@mainpage Calculator Application by Bharath G
+//@subpage calculator_operations.h
+
+void main()
+{
+ double val1 , val2;
+ char ope;
+
+ printf("Enter First Number ");
+ scanf("%lf", &val1);
+
+ printf("Enter Second Number ");
+ scanf("%lf", &val2);
+
+ printf("Enter '+' for Add\n '-' for Sub\n '*' for Mul\n '/' for Div\n");
+ scanf(" %c", &ope);
+
+ cal(val1,val2,ope); //calling function
+
+} //main end
